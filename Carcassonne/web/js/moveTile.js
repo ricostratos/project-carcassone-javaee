2b20d323aaa1b75ee8800269639e33d33524c99c
Digiboxi <digiboxi@gmail.com>
2013-11-22 18:03:36 +0200

    JQuery hapatusta satunnaisesti häpättäen
    Nyt pelilauta -tablessa voi olla myös negatiivisia koordinaatteja, ja
    palikan voi myös laittaa negatiivisiin koordinaatteihin!

--- Carcassonne/web/js/moveTile.js
+++ Carcassonne/web/js/moveTile.js
@@ -3,25 +3,31 @@ $(function() {
 });
 
 var newTileRotation = 0;
-var trueTileRotation;
+var trueTileRotation, coordinates;
 
 function checkNewTileLocation() {
-    var match = false;
-    for(var i=0; i<$("#gameBoardTable").find("tr").length; i++) {
-        for(var j=0; j<$("#gameBoardRow"+i).find("td").length; j++) {
-            if($("#"+i+"-"+j).attr("class").split(" ")[0] === "newTilePlaceHolder") {
-            if(Math.abs($("#newTile").position().left - $("#"+i+"-"+j).position().left) >= 50 || Math.abs($("#newTile").position().top - $("#"+i+"-"+j).position().top) >= 50) {
-                
-            } else {
+    var match = false;  // Tehdään tarkistin jolla tarkistetaan onko palikka löytänyt paikkansa.
+    var smallestX = $("#gameBoardTable").find("td").attr("id").split("_")[0]; // Haetaan pienimmän x-koordinaati
+    var smallestY = $("#gameBoardTable").find("td").attr("id").split("_")[1]; // Haetaan pienimmän y-koordinaatin itseisarvo
+    var temp_i = 0, temp_j = 0;
+    
+    for(var i=smallestX; i<$("#gameBoardTable").find("tr").length+parseInt(smallestX); i++) {
+        temp_i++;
+        for(var j=smallestY; j<parseInt($("#gameBoardRow"+i).find("td").length)+parseInt(smallestY); j++) {
+            temp_j++;
+            if($("#"+i+"_"+j).attr("class").split(" ")[0] === "newTilePlaceHolder") {
+            if(Math.abs($("#newTile").position().left - $("#"+i+"_"+j).position().left) < 50 && Math.abs($("#newTile").position().top - $("#"+i+"_"+j).position().top) < 50) {
                 $("#newTile").css("opacity","1");
-                $("#newTile").css("left",$("#"+i+"-"+j).position().left);
-                $("#newTile").css("top",$("#"+i+"-"+j).position().top);
-                rotatorVisible();
+                $("#newTile").css("left",$("#"+i+"_"+j).position().left);
+                $("#newTile").css("top",$("#"+i+"_"+j).position().top);
                 match = true;
+                coordinates = i+"_"+j;
+                rotatorVisible();
             }
             }
         }
     }
+    
     if(!match) {
         $("#newTile").css("opacity","1");
         $("#newTile").css("left",$("#gameInfo").position().left+1);
@@ -44,7 +50,7 @@ function newTileReady() {
     rotatorHidden();
     trueTileRotation = newTileRotation % 360;
     if(trueTileRotation < 0) trueTileRotation += 360;
-    alert("Palikka asetettu.\nRotation: "+newTileRotation+"\nmod(360): "+trueTileRotation);
+    alert("Palikka asetettu.\nRotation: "+newTileRotation+"\nmod(360): "+trueTileRotation+"\nKoordinaatit:\n X:"+coordinates.split("_")[0]+"\n Y:"+coordinates.split("_")[1]);
 }
 
 function rotateCW() {
