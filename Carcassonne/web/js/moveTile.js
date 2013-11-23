0fbb973f8a5848eca87b2542fc3d6ca42329f676
Digiboxi <digiboxi@gmail.com>
2013-11-23 13:48:08 +0200

    jQuery hapatuksen ja häpätyksen siivous
    Turhat vanhat kommenteissa olleet käyttämättömät koodipätkät bittien
    taivaaseen..

--- Carcassonne/web/js/moveTile.js
+++ Carcassonne/web/js/moveTile.js
@@ -13,7 +13,6 @@ function checkNewTileLocation() {
     
     for(var i=smallestY; i<$("#gameBoardTable").find("tr").length + parseInt(smallestY); i++) {
         for(var j=smallestX; j<$("#gameBoardRow"+i).find("td").length + parseInt(smallestX); j++) {
-            //alert("Rivi: "+i+"/"+$("#gameBoardTable").find("tr").length + parseInt(smallestY)+"\nSolu: "+j+"/"+$("#gameBoardRow"+i).find("td").length + parseInt(smallestX));
             if($("#"+j+"_"+i).attr("class").split(" ")[0] === "newTilePlaceHolder") {
             if(Math.abs($("#newTile").position().left - $("#"+j+"_"+i).position().left) < 51 &&
                Math.abs($("#newTile").position().top - $("#"+j+"_"+i).position().top) < 51) {
@@ -29,25 +28,6 @@ function checkNewTileLocation() {
         }
     }
     
-    /*
-    for(var i=smallestY; i<$("#gameBoardTable").find("tr").length+parseInt(smallestY); i++) {
-        temp_i++;
-        //alert($("#gameBoardTable").find("tr").length+parseInt(smallestX));
-        for(var j=smallestX; j<parseInt($("#gameBoardRow"+i).find("td").length)+parseInt(smallestX); j++) {
-            temp_j++;
-            if($("#"+i+"_"+j).attr("class").split(" ")[0] === "newTilePlaceHolder") {
-            if(Math.abs($("#newTile").position().left - $("#"+i+"_"+j).position().left) < 50 && Math.abs($("#newTile").position().top - $("#"+i+"_"+j).position().top) < 50) {
-                $("#newTile").css("opacity","1");
-                $("#newTile").css("left",$("#"+i+"_"+j).position().left);
-                $("#newTile").css("top",$("#"+i+"_"+j).position().top);
-                match = true;
-                coordinates = i+"_"+$("#gameBoardRow"+i).find("td").length+smallestY;
-                rotatorVisible();
-            }
-            }
-            alert(j+"/"+parseInt($("#gameBoardRow"+i).find("td").length)+parseInt(smallestY));
-        }
-    }*/
     if(!match) {
         $("#newTile").css("opacity","1");
         $("#newTile").css("left",$("#gameInfo").position().left+1);
@@ -78,18 +58,10 @@ function rotateCW() {
     $("#newTileImage").css("transform","rotate("+newTileRotation+"deg)");
     $("#newTileImage").css("-ms-transform","rotate("+newTileRotation+"deg)");
     $("#newTileImage").css("-webkit-transform","rotate("+newTileRotation+"deg)");
-    
-    /*if(newTileRotation >= 360) {
-        newTileRotation = 0;
-    }*/
 }
 function rotateCCW() {
     newTileRotation -= 90;
     $("#newTileImage").css("transform","rotate("+newTileRotation+"deg)");
     $("#newTileImage").css("-ms-transform","rotate("+newTileRotation+"deg)");
     $("#newTileImage").css("-webkit-transform","rotate("+newTileRotation+"deg)");
-    
-    /*if(newTileRotation < 0) {
-        newTileRotation = 270;
-    }*/
 }
