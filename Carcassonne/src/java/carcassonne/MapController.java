0d699788345bfa6475df9f046231d4a027b1eabd
vebzical <vebzical@hotmail.com>
2013-12-12 20:35:29 +0200

    Laattoja on nyt realistinen määrä

--- Carcassonne/src/java/carcassonne/MapController.java
+++ Carcassonne/src/java/carcassonne/MapController.java
@@ -30,7 +30,10 @@ public class MapController
     
     private Tile newTile;
     private boolean createTile = true;
-    ArrayList<Integer> arl = new ArrayList(Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23));
+    ArrayList<Integer> arl = new ArrayList(Arrays.asList(0,0,0,0,1,1,1,1,2,2,2,2,
+            3,3,4,5,5,5,6,6,6,6,6,7,7,8,9,9,9,10,10,10,10,11,11,12,12,12,13,13,13,
+            13,13,13,14,14,15,15,15,16,16,17,17,18,18,18,19,19,19,20,20,20,20,21,
+            21,22,22,22,23,23,23,23,23,23));
     
     public MapController(){
         ServletContext ctx = (ServletContext) FacesContext.getCurrentInstance()
@@ -313,7 +316,6 @@ public class MapController
         return palaKay;
     }
     public int getNewRandomInt(){
-        Random rnd = new Random();
         long seed = System.nanoTime();
         Collections.shuffle(arl, new Random(seed));
         int i = 0;
